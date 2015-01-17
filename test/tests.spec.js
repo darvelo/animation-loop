72a915f418b04fe974b79b846f90f5de93904d1e
David Arvelo <david@davidarvelo.com>
2015-01-16 22:18:44 -0500

    Add some initial tests

--- test/tests.spec.js
+++ test/tests.spec.js
@@ -4,13 +4,86 @@ var AnimationLoop = require('../.tmp/animation-loop');
 
 chai.should();
 
-describe('AnimationLoop', function () {
-    it('starts a test', function () {
-        expect(1);
+describe('AnimationLoop Creation', function () {
+    it('creates an AnimationLoop without the `new` operator', function () {
+        var a = AnimationLoop();
+        expect(a).to.be.an.instanceof(AnimationLoop);
+    });
+});
+
+describe('Options Scrubbing', function () {
+    var scrub;
+
+    before(function () {
+        scrub = AnimationLoop.prototype.getScrubbedOptions;
     });
 
-    it('fails a test', function () {
-        var one = 2;
-        one.should.equal(1);
+    it('turns a function into an options object', function () {
+        var options = function () {};
+        var obj = scrub(options);
+        expect(obj).to.be.an('object');
+        obj.should.have.property('render');
+        expect(obj.render).to.equal(options);
+    });
+
+    it('throws when not given a render function', function () {
+        expect(function () { scrub({}); }).to.throw(Error);
+    });
+
+    it('throws when not given a function or object', function () {
+        expect(function () { scrub();      }).to.throw(Error);
+        expect(function () { scrub(1);     }).to.throw(Error);
+        expect(function () { scrub([]);    }).to.throw(Error);
+        expect(function () { scrub(null);  }).to.throw(Error);
+        expect(function () { scrub('str'); }).to.throw(Error);
+    });
+
+    it('scrubs non-whitelisted properties', function () {
+        var options = {
+            a: 1,
+            b: 2,
+            c: 3,
+            render: function () {},
+        };
+
+        var obj = scrub(options);
+        expect(obj).to.deep.equal({ render: options.render });
+    });
+
+    it('passes whitelisted properties through', function () {
+        var options = {
+            before: function () {},
+            render: function () {},
+            done: function () {},
+            args: [1],
+            duration: 5000,
+            paused: true,
+            pauseThreshold: 50,
+            autonomous: true,
+        };
+
+        var obj = scrub(options);
+        expect(obj).to.not.equal(options);
+        expect(obj).to.deep.equal(options);
+    });
+
+    it('ensures args property becomes an array', function () {
+        var options = {
+            render: function () {},
+            args: [1],
+        };
+
+        var obj = scrub(options);
+        expect(obj).to.not.equal(options);
+        expect(obj).to.deep.equal(options);
+        expect(obj).to.have.ownProperty('args');
+        expect(obj.args).to.deep.equal([1]);
+
+        options.args = 1;
+        obj = scrub(options);
+        expect(obj).to.not.equal(options);
+        expect(obj).to.not.deep.equal(options);
+        expect(obj).to.have.ownProperty('args');
+        expect(obj.args).to.deep.equal([1]);
     });
 });
